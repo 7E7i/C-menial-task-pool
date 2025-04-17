3188d152bad70f441cc3d2dd58007b37786db95e
7E7i <unjunggo3693@gmail.com>
2025-04-17 14:42:00 +0900

    Add files via upload

---
+++ CharacterMoving.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Character;
+
+namespace Game.Character
+{
+    class CharacterMoving
+    {
+        private int redo = 0;
+        private int sides = 0;
+        private int ups = 0;
+
+        public void CharacterMove()
+        {
+            ConsoleKeyInfo Keyinfo;
+
+            do
+            {
+                Keyinfo = Console.ReadKey(true);
+                Console.Clear();
+
+                switch (Keyinfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        sides++;
+                        Console.SetCursorPosition(sides, ups);
+                        Console.Write("□");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        sides--;
+                        Console.SetCursorPosition(sides, ups);
+                        Console.Write("□");
+                        break;
+                    case ConsoleKey.UpArrow:
+                        ups--;
+                        Console.SetCursorPosition(sides, ups);
+                        Console.Write("□");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        ups++;
+                        Console.SetCursorPosition(sides, ups);
+                        Console.Write("□");
+                        break;
+                }
+            } while (redo == 0);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using Game.Character;
+
+namespace UnitMoving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CharacterMoving character = new CharacterMoving();
+            character.CharacterMove();
+        }
+    }
+}
